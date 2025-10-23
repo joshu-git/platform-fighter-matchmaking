f2c511698c0e2402fc5d4208690a0fbbe09868bc
joshu <joshuajackargent@hotmail.com>
2025-10-23 20:03:56 +0100

    Fix UWebSocketsTransport import

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const colyseus = require("colyseus");
-const { UWebSocketsTransport } = require("@colyseus/uwebsockets-transport");
+const colyseusTransport = require("@colyseus/uwebsockets-transport");
 const LobbyRoom = require("./rooms/LobbyRoom");
 const SmashRoom = require("./rooms/SmashRoom");
 
 const PORT = parseInt(process.env.PORT) || 2567;
 
 const gameServer = new colyseus.Server({
-  transport: new UWebSocketsTransport({ port: PORT }),
+  transport: new colyseusTransport.UWebSocketsTransport({ port: PORT }),
 });
 
 // Register rooms
